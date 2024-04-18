e22a20bcfceaf1fe748248f389a4a4f36825539f
simochee <lollipop@simochee.net>
2024-04-18 17:32:43 +0900

    build(css): setup tailwindcss 4

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,5 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "wxt";
 
 export default defineConfig({
+	vite: () => ({
+		plugins: [tailwindcss()],
+		css: {
+			transformer: "lightningcss",
+		},
+		build: {
+			cssMinify: "lightningcss",
+		},
+	}),
 	imports: false,
 });
