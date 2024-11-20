784fb3429d0c3a301a50b4bd0e3369f8f70dd068
simochee <lollipop@simochee.net>
2024-11-21 00:06:33 +0900

    chore: stories group

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,20 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 export default {
-	stories: ["../src/**/*.stories.tsx"],
+	stories: [
+		{
+			directory: "../components",
+			titlePrefix: "Shared/",
+		},
+		{
+			directory: "../entrypoints/options",
+			titlePrefix: "Options/",
+		},
+		{
+			directory: "../entrypoints/popup",
+			titlePrefix: "Popup/",
+		},
+	],
 	framework: "@storybook/react-vite",
 	addons: ["@storybook/addon-essentials"],
 	staticDirs: ["./public"],
